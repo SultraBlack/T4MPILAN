c9ae9ef25600ae734f4228cbf5d6ee80525e36f0
@DeonBlackHate <97623801+SultraBlack@users.noreply.github.com>
2022-01-13 10:09:20 +0700

    Add files via upload

---
+++ tampilan.sh
@@ -0,0 +1,35 @@
+#!bin/sh
+# Coded By : @Deon_BlackHate
+# script : Tampilan termux
+# tampilan
+tamp1"+=======================================+"
+tamp2" (  !Whe Are Login SultraBlackHate ! )"
+tamp3"    ( Author : @Deon_BlackHate )"
+tamp4"     ( Youtube :Deon_BlackHate )"
+tamp5"+=======================================+"
+# LOGIN EMOLATOR
+toilet -f big -F gay LOGIN
+echo "Masukan Username | lolcat
+read user
+#data Tampilan
+if [ $pass = DeonBlackHate ]
+then
+     echo"Masukan Password"
+     read pass
+     clear
+     figlet $pass lolcat
+     echo $tamp1 | lolcat #tampilan 1
+     echo $tamp2 | lolcat #tampilan 2
+     echo $tamp3 | lolcat #tampilan 3
+     echo $tamp4 | lolcat #tampilan 4
+     echo $tamp5 | lolcat #tampilan 5
+     echo
+     echo" <======= Sulawesi BlackHate =======>" | lolcat
+     echo" <======= Surabaya BlackHate =======>" | lolcat
+     echo" <======= Security Cyber ART =======>" | lolcat
+     echo $tamp1 | lolcat #tampilan 1
+     else
+     echo "Password Salah" | lolcat
+     echo $tamp1 | lolcat #tampilan 1
+     sh tampilan.sh
+fi
